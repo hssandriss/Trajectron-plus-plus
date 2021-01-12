14e5ad582e8597b7fb13589bddd659741afc8e46
Hssan Driss <drissh@informatik.uni-freiburg.de>
2021-01-12 21:21:03 +0100

    name_tag to losses csv

--- trajectron/train_multi.py
+++ trajectron/train_multi.py
@@ -463,8 +463,8 @@ def main():
 
         if args.save_every is not None and args.debug is False and epoch % args.save_every == 0:
             model_registrar.save_models(epoch)
-    train_loss_df.to_csv('train_loss.csv', sep=";")
-    eval_loss_df.to_csv('eval_loss.csv', sep=";")
+    train_loss_df.to_csv('train_loss%s.csv' % args.log_tag, sep=";")
+    eval_loss_df.to_csv('eval_loss%s.csv' % args.log_tag, sep=";")
 
 
 if __name__ == '__main__':
